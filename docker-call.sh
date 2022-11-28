21d3326070e49f230faa71ad471e5d30f00a37c8
face0b1101 <6190957+face0b1101@users.noreply.github.com>
2022-11-28 13:25:51 +0000

    working env var pipeline example

--- docker-call.sh
+++ docker-call.sh
@@ -1,6 +1,8 @@
 docker run \
 --rm \
 -it \
+--env-file .env \
+--env DOC_ID=XMswyH8BGr11RK4ldgiJ \
 --network local-logstash-testing \
---mount type=bind,source=$PWD/logstash.conf,destination=/usr/share/logstash/pipeline/logstash.conf \
+--mount type=bind,source=$PWD/pipeline/logstash.conf,destination=/usr/share/logstash/pipeline/logstash.conf \
 docker.elastic.co/logstash/logstash:8.5.1
