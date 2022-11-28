bf7f54e7d15335c7b1496ba612dcad1688868b7a
face0b1101 <6190957+face0b1101@users.noreply.github.com>
2022-11-28 15:08:11 +0000

    update logstash.conf + docker-call.sh

--- docker-call.sh
+++ docker-call.sh
@@ -2,7 +2,7 @@ docker run \
 --rm \
 -it \
 --env-file .env \
---env DOC_ID=XMswyH8BGr11RK4ldgiJ \
+--env DOC_ID='"XMswyH8BGr11RK4ldgiJ","9cswyH8BGr11RK4ldgaJ"' \
 --network local-logstash-testing \
 --mount type=bind,source=$PWD/pipeline/logstash.conf,destination=/usr/share/logstash/pipeline/logstash.conf \
 docker.elastic.co/logstash/logstash:8.5.1
